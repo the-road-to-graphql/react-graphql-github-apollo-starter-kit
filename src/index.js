5e02e2fda374c4989ca34a2322bb64b3e6378ef1
Robin Wieruch <wrobin@gmx.net>
2018-06-10 11:19:35 +0800

    add boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+
+import './style.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
